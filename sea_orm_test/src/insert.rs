dbf86ad39f7a3843448d4ed7b5a33cf9384d55e0
naonao2323 <yanagidanaoki9900@gmail.com>
2024-09-08 00:37:58 +0900

    feat: sea orm transaction

--- sea_orm_test/src/insert.rs
+++ sea_orm_test/src/insert.rs
@@ -1,7 +1,7 @@
 use sea_orm::DatabaseConnection;
 use sea_orm::prelude::*;
 use sea_orm::{EntityTrait, Set};
-
+use sea_orm::TransactionTrait;
 
 use crate::entities::users::Entity as Users;
 use crate::entities::users::ActiveModel as UsersModel;
@@ -27,4 +27,44 @@ pub async fn create_comments(conn: &DatabaseConnection) {
     };
 
     let _ = Comments::insert(comments).exec(conn).await;
+}
+
+pub async fn create_user_and_comment_v1(conn: &DatabaseConnection, user_id: i32, name: String, manager_id: i32, comment_id: i32) {
+    conn.transaction::<_, (), DbErr>(|tx| {
+        Box::pin(async move {
+            let user: UsersModel = UsersModel {
+                id: Set(user_id.to_owned()),
+                name: Set(name.to_owned()),
+                manager_id: Set(manager_id.to_owned())
+            };
+            let _ = Users::insert(user).exec(tx).await;
+            let comments = CommentsModel {
+                test: Set("test".to_string().to_owned()),
+                user_id: Set(user_id.to_owned()),
+                id: Set(comment_id.to_owned())
+            };
+        
+            let _ = Comments::insert(comments).exec(tx).await;
+            Ok(())
+        })
+    })
+    .await;
+}
+
+pub async fn create_user_and_comment_v2(conn: &DatabaseConnection, user_id: i32, name: String, manager_id: i32, comment_id: i32) {
+    let tx = conn.begin().await.expect("fail to get transaction");
+    let user: UsersModel = UsersModel {
+        id: Set(user_id.to_owned()),
+        name: Set(name.to_owned()),
+        manager_id: Set(manager_id.to_owned())
+    };
+    let _ = Users::insert(user).exec(&tx).await;
+    let comments = CommentsModel {
+        test: Set("test".to_string().to_owned()),
+        user_id: Set(user_id.to_owned()),
+        id: Set(comment_id.to_owned())
+    };
+    let _ = Comments::insert(comments).exec(&tx).await;
+    let _ = tx.commit().await;
+
 }
--- sea_orm_test/src/main.rs
+++ sea_orm_test/src/main.rs
@@ -1,14 +1,16 @@
-use sea_orm_test::{connection, delete::delete_user, get::get_users, insert::{create_comments, create_user}, select::{self, select_comments_by_v2, select_comments_by_v1}, update::update_user};
+use sea_orm_test::{connection, delete::delete_user, get::get_users, insert::{create_comments, create_user, create_user_and_comment_v1, create_user_and_comment_v2}, select::{self, select_comments_by_v2, select_comments_by_v1}, update::update_user};
 
 #[tokio::main]
 async fn main() {
     let pool = connection::new_poll().await.expect("fail to get connnection pool");
 
-    let _ = create_user(&pool, 1, "test".to_string(), 1).await;
+    // let _ = create_user(&pool, 1, "test".to_string(), 1).await;
 
-    let _ = update_user(&pool, 1, "test2".to_string()).await;
+    // let _ = update_user(&pool, 1, "test2".to_string()).await;
 
-    let _ = create_comments(&pool).await;
+    // let _ = create_comments(&pool).await;
+
+    let _ = create_user_and_comment_v2(&pool, 1, "test".to_string(), 1, 1).await;
 
     let users = get_users(&pool).await;
     print!("{:?}", users);
